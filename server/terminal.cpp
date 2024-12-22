ece68b8d68a3ca55e95674993480b39e35f7fbd0
David Houdek <david.houdek11@gmail.com>
2024-12-22 20:57:26 +0100

    docker: needed something to trigger workflow

--- server/terminal.cpp
+++ server/terminal.cpp
@@ -3,20 +3,17 @@
 
 #define _terminal "terminal: "
 
-inline void printT(const std::string& message){
-	std::cout << _terminal << message << std::endl;
-}
+inline void printT ( const std::string& message ) { std::cout << _terminal << message << std::endl; }
 
-inline void terminal(std::condition_variable & callBack, bool & turnOff) {
+inline void terminal ( std::condition_variable& callBack, bool& turnOff ) {
 	while ( !turnOff ) {
 		std::string input;
 		std::cin >> input;
-		if(input == "q") {
+		if ( input == "q" ) {
 			turnOff = true;
 			printT("turning off server, please wait up to 15 seconds");
 			callBack.notify_one();
 			return;
 		}
 	}
-
 }
