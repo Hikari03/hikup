1d70b0b83aeaaf6f18703dc13d12214f245632c6
David Houdek <david.houdek11@gmail.com>
2026-02-04 13:41:46 +0100

    server: wrong namespace

--- src/server/utils.cpp
+++ src/server/utils.cpp
@@ -13,25 +13,24 @@ std::string operator+ ( const std::string& lhs, const toml::source_position& rhs
     return rhs1.str();
 }
 
+namespace Utils {
+    void log ( const std::string& message, const bool newline ) {
+        std::lock_guard lock(_logMutex);
+        std::cout << message;
+        if ( newline )
+            std::cout << std::endl;
+        else
+            std::cout << std::flush;
+    }
 
-void log ( const std::string& message, const bool newline ) {
-    std::lock_guard lock(_logMutex);
-    std::cout << message;
-    if ( newline )
-        std::cout << std::endl;
-    else
-        std::cout << std::flush;
+    void elog ( const std::string& message, const bool newline ) {
+        std::lock_guard lock(_logMutex);
+        std::cerr << message;
+        if ( newline )
+            std::cerr << std::endl;
+        else
+            std::cerr << std::flush;
+    }
 }
-
-void elog ( const std::string& message, const bool newline ) {
-    std::lock_guard lock(_logMutex);
-    std::cerr << message;
-    if ( newline )
-        std::cerr << std::endl;
-    else
-        std::cerr << std::flush;
-}
-
-
 
 
